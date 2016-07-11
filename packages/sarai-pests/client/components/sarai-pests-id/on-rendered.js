798c219d2fef65b3a5b272da8008ea483b29c33f
jfcanaya <jccanaya2@up.edu.ph>
2016-07-11 16:02:48 +0800

    Train Pests

--- packages/sarai-pests/client/components/sarai-pests-id/on-rendered.js
+++ packages/sarai-pests/client/components/sarai-pests-id/on-rendered.js
@@ -1,3 +1,3 @@
 Template.SaraiPestsId.onRendered(function() {
-	$('.jqDropZone').html("<img src='/images/drop-here.png' width='100%' height='295px'/>");
+	$('.jqDropZone').html("<img src='/drop-here.png' width='100%' height='295px'/>");
 })
