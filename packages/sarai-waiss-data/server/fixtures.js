188090d2af61e266c2389c4ca9efcd38cac636e3
richardpitargue <jlpitargue@up.edu.ph>
2016-07-11 14:18:40 +0800

    Update farm fixtures

--- packages/sarai-waiss-data/server/fixtures.js
+++ packages/sarai-waiss-data/server/fixtures.js
@@ -1,21 +1,28 @@
 if(Farm.find().count() === 0) {
-    var userId = 'CQYQGAmy6wi3phsos';
+    const userId = 'vcjfTseG2NyNuDG8t';
+    const aMonthAgo = new Date();
+    const aWeekAgo = new Date();
+
+    aMonthAgo.setMonth(aMonthAgo.getMonth() - 1);
+    aWeekAgo.setDate(aWeekAgo.getDate() - 7);
 
     Farm.insert({
-        userId: userId,
-        name: 'JC\'s Corn Farm',
-        crop: 'Corn',
-        variety: 'Sweet',
-        location: 'Silangan Road, UPLB, Laguna',
-        soilType: 'loam'
+        'userId': userId,
+        'name': 'My Sweet Corn Farm',
+        'crop': 'Corn',
+        'variety': 'Sweet Corn',
+        'plantingDate': aMonthAgo,
+        'location': 'Los Baños, Laguna',
+        'soilType': 'loam'
     });
 
     Farm.insert({
-        userId: userId,
-        name: 'JC\'s Rice Farm',
-        crop: 'Rice',
-        variety: 'Sticky',
-        location: 'Silangan Road, UPLB, Laguna',
-        soilType: 'loam'
+        'userId': userId,
+        'name': 'My Banana Farm',
+        'crop': 'Banana',
+        'variety': 'Lakatan',
+        'plantingDate': aWeekAgo,
+        'location': 'Los Baños, Laguna',
+        'soilType': 'loam'
     });
 }
