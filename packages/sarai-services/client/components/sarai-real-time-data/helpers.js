5ea92bac7207ce0027d25aa8ce71f1fed81d0a94
dsdinglasan <Veigny@gmail.com>
2016-07-12 17:13:34 +0800

    Removed the unfinished package

--- packages/sarai-services/client/components/sarai-real-time-data/helpers.js
+++
@@ -1,6 +0,0 @@
-/*Template.SaraiHome.helpers({
-  title: function(){
-    //return Home.find({'title': 'Hello World'});
-    return Main.findOne();
-  }
-})*/
--- packages/sarai-services/client/components/sarai-real-time-data/on-created.js
+++
@@ -1,5 +0,0 @@
-Template.SaraiHome.onCreated(function() {
-  Meteor.subscribe('main');
-  Meteor.subscribe('services');
-  Meteor.subscribe('blog');
-})
