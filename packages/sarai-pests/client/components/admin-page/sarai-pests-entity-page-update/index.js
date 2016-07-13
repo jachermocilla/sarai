c3be82a876b30071e9b616165e975211d9823d96
jcmangaliag <mangaliagj@gmail.com>
2016-07-14 06:12:21 +0800

    Added functional Pest Edit

--- packages/sarai-pests/client/components/admin-page/sarai-pests-entity-page-update/index.js
+++ packages/sarai-pests/client/components/admin-page/sarai-pests-entity-page-update/index.js
@@ -22,24 +22,38 @@ Template.EntityPageUpdate.events({
 	'click #uBtn': function(e){
 			e.preventDefault();
 			
-			/*Get Value*/
-			var name = $("#name").val();
-			var type = $("#type").val();
-			var commonNames = $("#common-names").val();
-			var filipinoNames = $("#filipino-names").val();
-			var scientificName = $("#scientific-name").val();
+			var pestData = {
+				id: $("#id").val(),
+				name: $("#name").val(),
+				type: $("#type").val(),
+				eng_name: $("#common-names").val(),
+				fil_name: $("#filipino-names").val(),
+				sci_name: $("#scientific-name").val(),
+				order: $("#order").val(),
+				classification: $("#classification").val(),
+				stage_threatening: $("#threatening-stage").val(),
+				plant_affected: $("#plant-affected").val(),
+				description: $("#description").val(),
+				symptoms: $("#symptoms").val(),
+				effect: $("#effect").val(),
+				treatment: $("#treatment").val(),
+				stage_plant_affected: $("#stage-plant-affected").val(),
+				part_destroyed: $("#part-destroyed").val(),
+				fil_stage_plant_affected: $("#fil-stage-plant-affected").val(),
+				fil_effect: $("#fil-effect").val(),
+				fil_part_destroyed: $("#fil-part-destroyed").val(),
+				fil_stage_threatening: $("#fil-threatening-stage").val(),
+				fil_symptoms: $("#fil-symptoms").val(),
+				fil_description: $("#fil-description").val(),
+				fil_plant_affected: $("#fil-plant-affected").val(),
+				fil_classification: $("#fil-classification").val(),
+				fil_treatment: $("#fil-treatment").val()
+			};
 			
-			//Meteor.call('updateMonitor', bhead, bsubhead);
-			
+			Meteor.call('updatePest', pestData);
 			console.log("inserted");
-			$('#viewChanges').show();
 		},
 	'click #cBtn': function(e){
 			e.preventDefault();
-		},
-	'click #viewChanges': function(e){
-			e.preventDefault();
-			FlowRouter.go("/pests-monitor");
 		}
-
 });
