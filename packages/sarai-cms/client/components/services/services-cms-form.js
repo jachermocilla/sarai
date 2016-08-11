31721eff68bfe5de623078367a7855be1b024248
Monina Carandang <reactor04@gmail.com>
2016-08-11 13:13:34 +0800

    Initial layout for services form

--- packages/sarai-cms/client/components/services/services-cms-form.js
+++ packages/sarai-cms/client/components/services/services-cms-form.js
@@ -6,9 +6,12 @@ Template.ServicesCMSForm.onCreated(() => {
 Template.ServicesCMSForm.onRendered(() => {
   setFormDirty()
 
-  $(document).ready(function() {
-    $('#summernote').summernote();
-  });
+  $('#cms-service-col1text-editor').summernote();
+  $('#cms-service-col2text-editor').summernote();
+
+  //This is such a hack
+  const hiddenCode = $('#hidden-col1text').attr('value')
+  $('#cms-service-col1Text-editor').code(hiddenCode)
 })
 
 Template.ServicesCMSForm.events({
@@ -32,6 +35,10 @@ Template.ServicesCMSForm.helpers({
 const setFormDirty = () => {
 
   $('#cms-service-title').addClass('is-dirty')
+  $('#cms-service-tagline').addClass('is-dirty')
+  $('#cms-service-crops').addClass('is-dirty')
+  $('#cms-service-experts').addClass('is-dirty')
+  $('#cms-service-ura').addClass('is-dirty')
 }
 
 
