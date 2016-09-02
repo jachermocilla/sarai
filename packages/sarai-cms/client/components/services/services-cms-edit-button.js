6fab6b4dc8692c8cdbe32523a187c072ba940d1a
Monina Carandang <reactor04@gmail.com>
2016-09-02 17:47:59 +0800

    Worked on services

--- packages/sarai-cms/client/components/services/services-cms-edit-button.js
+++ packages/sarai-cms/client/components/services/services-cms-edit-button.js
@@ -2,6 +2,20 @@ Template.ServicesCMSEditButton.onRendered(() => {
 
 })
 
+<<<<<<< cfa99f480272cde31853b973137639f76c3fa8f2
+=======
+Template.ServicesCMSEditButton.events({
+  'click #cms-service-edit-button': () => {
+
+  },
+
+  'click .cms-service-edit': (event, template) => {
+    console.log('Clicked edit button')
+    console.log(template.data.id)
+
+}})
+
+>>>>>>> Worked on services
 Template.ServicesCMSEditButton.helpers({
   edit: () => {
 
