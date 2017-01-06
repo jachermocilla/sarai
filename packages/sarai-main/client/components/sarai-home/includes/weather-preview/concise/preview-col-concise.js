91333d76515e2ad939db0220a3305260fdca2966
Monina Carandang <reactor04@gmail.com>
2017-01-06 12:26:33 +0800

    Started fixing 30-day rainfall fetch error

--- packages/sarai-main/client/components/sarai-home/includes/weather-preview/concise/preview-col-concise.js
+++ packages/sarai-main/client/components/sarai-home/includes/weather-preview/concise/preview-col-concise.js
@@ -1,5 +1,9 @@
 Template.PreviewColConcise.onCreated(() => {
-  Meteor.subscribe('weather-data-30')
+  Meteor.subscribe('weather-data-30', () => {
+    console.log("subscriptions finished")
+    const data = WeatherData.find({})
+    console.log(data.fetch())
+  })
 })
 
 Template.PreviewColConcise.onRendered(() => {
@@ -10,8 +14,8 @@ Template.PreviewColConcise.helpers({
   accumulatedRain: (stationID) => {
     const weatherData = WeatherData.find({id: stationID})
 
-    console.log(stationID)
-    console.log(weatherData.fetch())
+    // console.log(stationID)
+    // console.log(weatherData.fetch())
 
     if (weatherData) {
       const rainfall = Meteor.previewHelpers.get30DayRainfall(weatherData.fetch())
