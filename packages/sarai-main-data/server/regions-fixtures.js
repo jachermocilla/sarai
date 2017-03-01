047354ff9a3342b87daf18a178bd607877cfcb10
Erin Cuenco <jem.cuenco@gmail.com>
2017-03-02 00:21:41 +0800

    Added dropdown for regions in rainfall outlook

---
+++ packages/sarai-main-data/server/regions-fixtures.js
@@ -0,0 +1,74 @@
+if (Regions.find().count() === 0) {
+	Regions.insert({
+		region: "Negros Island Region",
+		province: ["Negros Occidental","Negros Oriental"]
+	});
+	Regions.insert({
+		region: "National Capital Region",
+		province: ["Metropolitan Manila"]
+	});
+	Regions.insert({
+		region: "Cordillera Administrative Region",
+		province: ["Abra","Apayao","Benguet","Ifugao","Kalinga","Mountain Province"]
+	});
+	Regions.insert({
+		region: "Region I: Ilocos Region",
+		province: ["Ilocos Norte","Ilocos Sur","La Union","Pangasinan"]
+	});
+	Regions.insert({
+		region: "Region II: Cagayan Valley",
+		province: ["Batanes","Cagayan","Isabela","Nueva Vizcaya","Quirino"]
+	});
+	Regions.insert({
+		region: "Region III: Central Luzon",
+		province: ["Aurora","Bataan","Bulacan","Nueva Ecija","Pampanga","Tarlac","Zambales"]
+	});
+	Regions.insert({
+		region: "Region IV-A: CALABARZON",
+		province: ["Batangas","Cavite","Laguna","Quezon","Rizal"]
+	});
+	Regions.insert({
+		region: "Region IV-B: MIMAROPA",
+		province: ["Marinduque","Occidental Mindoro","Oriental Mindoro","Palawan","Romblon"]
+	});
+	Regions.insert({
+		region: "Region V: Bicol Region",
+		province: ["Albay","Camarines Norte","Camarines Sur","Catanduanes","Masbate","Sorsogon"]
+	});
+	Regions.insert({
+		region: "Region VI: Western Visayas",
+		province: ["Aklan","Antique","Capiz","Guimaras","Iloilo"]
+	});
+	Regions.insert({
+		region: "Region VII: Central Visayas",
+		province: ["Bohol","Cebu","Siquijor"]
+	});
+	Regions.insert({
+		region: "Region VIII: Eastern Visayas",
+		province: ["Biliran","Eastern Samar","Leyte","Northern Samar","Samar","Southern Leyte"]
+	});
+	Regions.insert({
+		region: "Region IX: Zamboanga Peninsula",
+		province: ["Zamboanga del Norte","Zamboanga del Sur","Zamboanga Sibugay"]
+	});
+	Regions.insert({
+		region: "Region X: Northern Mindanao",
+		province: ["Bukidnon","Camiguin","Lanao del Norte","Lanao del Sur","Misamis Occidental","Misamis Oriental"]
+	});
+	Regions.insert({
+		region: "Region XI: Davao Region",
+		province: ["Compostela Valley","Davao del Norte","Davao del Sur","Davao Oriental"]
+	});
+	Regions.insert({
+		region: "Region XII: SOCCSKSARGEN",
+		province: ["North Cotabato","Sarangani","South Cotabato","Sultan Kudarat"]
+	});
+	Regions.insert({
+		region: "Region XIII: Caraga",
+		province: ["Agusan del Norte","Agusan del Sur","Dinagat Islands","Surigao del Norte","Surigao del Sur"]
+	});
+	Regions.insert({
+		region: "Autonomous Region in Muslim Mindanao",
+		province: ["Basilan","Maguindanao","Shariff Kabunsuan","Sulu","Tawi-Tawi"]
+	});
+}
