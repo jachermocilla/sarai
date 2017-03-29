df57e230e6f91e7104dfc5eaadae8656a112a449
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-03-29 12:07:43 +0800

    Small modifications (#46)
    * Added rainfall outlook and description for expected rainfall
    
    * Started fixing 30-day rainfall fetch error
    
    * Removed gitignore file for icm images
    
    * Fixed new year bug associated with fetching weather day for past 30 days
    
    * Updated files
    
    * Fixed responsiveness of rainfall outlook
    
    * Added rainfall outlook for June
    
    * Added function to download weather forecast
    
    * Updated files
    
    * Temporarily removed download button for expected rainfall
    
    * Updated files
    
    * Updated files
    
    * Updated files
    
    * Update forecast-view.html
    
    * Added graph view for past 30-day rainfall
    
    * Restored part of code
    
    * Fixed graph view
    
    * Removed rainfall outlook for February and added rainfall outlook for July
    
    * Modified description of rainfall outlook
    
    * Modified description of rainfall outlook
    
    * Modified description of rainfall outlook
    
    * Modified description of rainfall outlook
    
    * Added dropdown for regions in rainfall outlook
    
    * Modified regions' fixtures
    
    * Modified
    
    * Modified regions' fixtures
    
    * Modified description of Rainfall Outlook
    
    * Added 'All' for the 3 dropdowns in rainfall outlook
    
    * Changed type of graph in graph view and modified some files
    
    * Modified description of expected rainfall
    
    * Small modification
    
    * Whole number for rainfall outlook
    
    * Small modifications
    
    * Small modifications

--- packages/sarai-about-data/server/fixtures.js
+++ packages/sarai-about-data/server/fixtures.js
@@ -30,50 +30,67 @@ if (About.find().count() === 0) {
 	      	href: '',
 	      	links: [
 	      		{
-	      			name: 'Suitability Maps',
-	      			href: 'http://159.203.253.36:8081/web/'
-	      		},
-	      		{
-	      			name: 'Weather Monitoring',
-	      			href: '/explore/weather-monitoring'
+	      			name: 'Accumulated Rainfall',
+	      			href: '/accumulated-rainfall'
 	      		},
 	      		{
 	      			name: 'Crop Production Area',
-	      			href: '/explore/crop-production-area'
+	      			href: 'http://159.203.253.31/crop-production-area/rice'
 	      		},
 	      		{
 	      			name: 'Normalized Difference Vegetation Index (NDVI)',
-	      			href: '/explore/ndvi'
-	      		},
-	      		{
-	      			name: 'Rainfall',
-	      			href: '/explore/rainfall'
+	      			href: 'http://159.203.253.31/ndvi/2016-12-01/2016-12-31?province=Laguna&center=14.270005%2C121.335777974'
 	      		},
 	      		{
 	      			name: 'Open Data',
-	      			href: 'http://202.92.144.42/'
+	      			href: 'http://opendata.sarai.ph/'
+	      		},
+	      		{
+	      			name: 'Rainfall Map',
+	      			href: 'http://159.203.253.31/rainfall-maps/2016-12-01/2016-12-31'
+	      		},
+	      		{
+	      			name: 'Suitability Maps',
+	      			href: 'http://159.203.253.31/suitability-maps/rice'
+	      		},
+	      		{
+	      			name: 'Weather Monitoring',
+	      			href: '/weather-monitoring'
 	      		}
 	      	],
 	      	id: 'explore-link',
 	      	withsublinks: true
 	      },
 	      {
-	      	name: 'Planning Dates',
-	      	href: '',
-	      	links: [],
-	      	id: 'planning-dates-link',
-	      	withsublinks: false
-	      },
-	      {
-	      	name: 'DSS',
+	      	name: 'Be Informed',
 	      	href: '',
 	      	links: [
 	      		{
+	      			name: '30-day cummulative rainfall + 10-day weather forecast',
+	      			href: 'http://10.0.3.131:3000/dss'
+	      		},
+	      		{
 	      			name: 'Corn Nutrient Expert',
-	      			href: '/dss/corn-nutrient-expert'
+	      			href: 'http://software.ipni.net/article/nutrient-expert'
+	      		},
+	      		{
+	      			name: 'Integrated Pest Management',
+	      			href: 'http://pests.sarai.ph/'
+	      		},
+	      		{
+	      			name: 'News and Events',
+	      			href: 'http://eskwela.sarai.ph/index.php?option=com_k2&view=itemlist&task=category&id=9&Itemid=322'
+	      		},
+	      		{
+	      			name: 'Planting Dates',
+	      			href: 'http://202.92.144.44/index.php?option=com_content&view=article&id=64&Itemid=321'
+	      		},
+	      		{
+	      			name: 'SARAi Eskwela',
+	      			href: 'http://eskwela.sarai.ph/index.php?option=com_content&view=article&id=38&Itemid=319'
 	      		}
 	      	],
-	      	id: 'dss-link',
+	      	id: 'be-informed-link',
 	      	withsublinks: true
 	      },
 	      {
@@ -81,16 +98,16 @@ if (About.find().count() === 0) {
 	      	href: '',
 	      	links: [
 	      		{
-	      			name: 'Contact Us',
-	      			href: '/get-involved/contact'
-	      		},
-	      		{
-	      			name: 'Submit a Story',
-	      			href: '/get-involved/contact'
+	      			name: 'Blogs',
+	      			href: 'http://eskwela.sarai.ph/index.php?option=com_k2&view=itemlist&layout=category&Itemid=320'
 	      		},
 	      		{
 	      			name: 'Join the Discussion',
 	      			href: 'https://groups.google.com/forum/#!forum/project-sarai-knowledge-hub'
+	      		},
+	      		{
+	      			name: 'Subscribe',
+	      			href: 'https://docs.google.com/forms/d/e/1FAIpQLSdRWCollMXmPGCoKhWZToL249RCc1FAh9KssEuCDH1jDj8ERw/viewform?c=0&w=1'
 	      		}
 	      	],
 	      	id: 'get-involved-link',
@@ -104,7 +121,7 @@ if (About.find().count() === 0) {
 
 	About.insert({
 		name: 'footer',
-		text: 'Project SARAI aims to develop a national crop forecasting and monitoring system for first six priority crops: rice, corn, banana, coconut, coffee, cacao.',
+		text: 'Project SARAi aims to develop a national crop forecasting and monitoring system for first six priority crops: rice, corn, banana, coconut, coffee, cacao.',
 		img: 'http://sarai.ph/wp-content/uploads/2016/03/DOST_PCAARRD_UPLB.png',
 		share: [
 	        {
@@ -114,7 +131,7 @@ if (About.find().count() === 0) {
 	        },
 	        {
 	            class_name: 'twt',
-	            href: 'https://twitter.com/ProjectSARAI',
+	            href: 'https://twitter.com/projectsarai_ph',
 	            img: '/footer-icons/twitter.png'
 	        },
 	        {
@@ -170,7 +187,7 @@ if (About.find().count() === 0) {
 	About.insert({
 		name: 'origin',
 		title: 'Our Story',
-	    description: '<p>So here’s a story of how the team came up with SARAI. It did not come easy, just as how other ideas did not come over night, the team had to go through to numerous discussions and brainstorming sessions before they reach the eureka moment. Sarai was Abraham’s wife, and she was the first character in the Bible who was said to be barren. But then later on, God chose them to be the forefathers of the nation of Israel. <br> It was a miracle, of how Sarai was able to conceive. So the team thought it apt to adopt the name, thus the name Project SARAI came about. Project SARAI hopes to bring about development in our agriculture sector by providing our farmers with smarter options. Also, Project SARAI hopes to help in making a more resilient and a more proactive agriculture sector by using state-of-the-art technologies, and providing near real-time crop advisories.<br> It is a long work in progress, but with the sustained partnership with different State Universities and Colleges (SUCs) nationwide, different government agencies, and other institutions, the solutions are well along the way to reaching the farmers and other stakeholders.</p>',
+	    description: '<p>So here’s a story of how the team came up with SARAi. It did not come easy, just as how other ideas did not come over night, the team had to go through to numerous discussions and brainstorming sessions before they reach the eureka moment. Sarai was Abraham’s wife, and she was the first character in the Bible who was said to be barren. But then later on, God chose them to be the forefathers of the nation of Israel. <br> It was a miracle, of how Sarai was able to conceive. So the team thought it apt to adopt the name, thus the name Project SARAi came about. Project SARAi hopes to bring about development in our agriculture sector by providing our farmers with smarter options. Also, Project SARAi hopes to help in making a more resilient and a more proactive agriculture sector by using state-of-the-art technologies, and providing near real-time crop advisories.<br> It is a long work in progress, but with the sustained partnership with different State Universities and Colleges (SUCs) nationwide, different government agencies, and other institutions, the solutions are well along the way to reaching the farmers and other stakeholders.</p>',
 		img: '/about/about-sarai-consult.jpg',
 		img2: '/services/sarai-eskwela.jpg',
 	    enabled: true,
@@ -192,15 +209,15 @@ if (About.find().count() === 0) {
 		        subtext2:'<p>Project 2 aims to provide the near real-time, empirical field data as inputs to the crop models simulated by Project 1. It also aims to provide site-specific nutrient management advisories for the perennials.</p>',
 				description2:'<br>– Updated land use and crop suitability maps<br>– Real time and edaphic data from Automatic Weather Stations (AWS) and sensors<br>– Integrated Crop Management System (ICMS)<br>– Vulnerability Studies</p>',
 
-		      	text3: '<b>Project 3: Sarai Knowledge Portal</b>',
-		        subtext3:'<p>Project 3 is the online infrastructure of Project SARAI which serves as the database, and the online go-to site of various stakeholders.</p>',
-				description3:'<p>– Database platform for Project SARAI outputs<br>– Crop advisories, crop forecasts, crop EWS<br>– Information/learning tools<br>– Planting calendar, ICM, and integrated water management IPM<p>',
+		      	text3: '<b>Project 3: SARAi Knowledge Portal</b>',
+		        subtext3:'<p>Project 3 is the online infrastructure of Project SARAi which serves as the database, and the online go-to site of various stakeholders.</p>',
+				description3:'<p>– Database platform for Project SARAi outputs<br>– Crop advisories, crop forecasts, crop EWS<br>– Information/learning tools<br>– Planting calendar, ICM, and integrated water management IPM<p>',
 
 		      	text4: '<b>Project 4: Capacity and Knowledge-Building</b>',
 		        subtext4:'<p>Project 4 makes sure that the system and the outputs of the research program are shared with the stakeholders through conducting technical trainings, seminars, workshops; developing training modules; and designing various IEC materials.</p>',
 				description4:'<p>– Analyzed training needs<br>– New and complementary training tools and activities<br>– Conduct of trainings, IEC ad joint learning activities on smarter farming techniques, ICM, DSS, AWS/Sensors</p>',
 
-		      	text5: '<b>Project 5: Sarai Mainstreaming</b>',
+		      	text5: '<b>Project 5: SARAi Mainstreaming</b>',
 		        subtext5:'Project 5 is in charge of the formulation of science-based recommendations for the agricultural sector. It also makes sure that the partner SUCs are committed into contributing to the consortium, and that the relevant government agencies will take part in building and improving the system.',
 				description5:'<p>– Identification of policy issues and formulation of policy papers<br>– Publication of research results<br>– Organization of planning and scientific meetings<br>– Establish: Crop-climate forecasting and modelling laboratory<br>– Build consortium to ensure sustainability</p>',
 
